000820ad0a928ed435cbca70caa261df3cd1aa05
artemO110 <doooober110@gmail.com>
2016-04-21 21:56:21 +0300

    home

---
+++ js/js_1-2/js2/names.js
@@ -0,0 +1,28 @@
+var names  = [];
+
+for (var i = 0; i < 5; i++) {
+	names.push(prompt('enter your name'));
+	console.log(names)
+}
+var userName = prompt('Enter username please');
+
+
+var rightName = false;
+
+
+for (var i = 0; i < names.length; i++) {
+
+	var nameItem = names[i];
+
+	if (nameItem == userName) {
+		var rightName = true;
+		alert('Congratulations ' + userName + '! Login succesfull')
+		break
+	} 
+
+}
+
+
+if (!rightName) {
+	alert('Error! No user found!')
+}
